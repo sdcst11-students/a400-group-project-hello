c11f24cec8f7b4099669e27d3af91e9e85c17ee0
Bellybrooks <ellejbrooks06@gmail.com>
2023-11-14 12:23:17 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,7 +2,7 @@
 # Volume Calculator
 # Feel free to rename your variables
 # Authers: Talan Elle
-
+import math
 def title():
     print("===Title Screen===")
     print("1.Show Instuctions")
@@ -39,7 +39,7 @@ def getRectangleValues():
             if -10000 < l < 10000:
                 break
         except:
-            print("Invaled input please enter in a integer.")
+            print("Invalid input please enter in a integer.")
         else:
             print("Value exceds for digits.")
     while True:
@@ -49,7 +49,7 @@ def getRectangleValues():
             if -10000 < w < 10000:
                 break
         except:
-            print("Invaled input please enter in a integer.")
+            print("Invalid input please enter in a integer.")
         else:
             print("Value exceds for digits.")
     while True:
@@ -59,7 +59,7 @@ def getRectangleValues():
             if -10000 < h < 10000:
                 break
         except:
-            print("Invaled input please enter in a integer.")
+            print("Invalid input please enter in a integer.")
         else:
             print("Value exceds for digits.")
     return l, w, h
@@ -69,8 +69,43 @@ def RectangleVolume():
     m = input("Enter units: ")
     v = l * w * h
     print(f"the volume is {v}{m}3")
-    
 
+def CubeLength():
+    while True:
+        vol=input("Enter the volume of the cube:")
+        try:
+            vol=float(vol)
+            break
+        except:
+            print("oops! Invalid input please enter in a integer.")
+        
+    length=vol**(1/3)   
+    print("the length of the cube is",length)
+
+def CylinderVolume():
+    while True:
+        try:
+            radius=int(input("Enter the radius of the cylinder:"))
+            height=int(input("Enter the height of cylinder:"))
+            volume=math.pi*(radius**2)*height
+            print("the volume of the cylinder is:",volume)
+            break
+        except:
+            print("oops! Invalid input please enter in a integer.")
+    
+def rectangularpyramid():
+    while True:
+        try:
+            length=int(input("enetr the lenth of the pyramid:"))
+            width=int(input("enetr the width of the pyramid:")) 
+            height=int(input("enetr the height of the pyramid:"))
+            volume=(length*width*height)/3
+            print("the volume of rectangular pyramid is:",volume)
+            break
+        except:
+            print("oops! Invalid input please enter in a integer.")
+
+def pythagoreanTheorem():
 def main():
     """
     main block of code that will run your program and control program flow
@@ -79,14 +114,10 @@ def main():
     """
     while True:
         title()
+        rectangularpyramid()
         # keep giving options to choose menu options until they choose to exit
         pass
 
+
 if __name__ == "__main__":
     main()
-
-
-
-vol=float(input("enter the volume of the cube:"))
-length=vol**(1/3)
-print("the length of tghe cube is",length)
